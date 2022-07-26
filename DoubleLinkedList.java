6b4b7a9688a3e2bfc56c51eeb95e414b015e6544
Priyank Thakur <priyankthakur3@live.com>
2022-07-26 18:37:05 -0400

    Added Additional functions for Double LinkedList

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,3 +1,7 @@
+/*
+ * Generic Dynamic Array Implementation
+ * Author: Priyank Thakur
+ */
 
 public class DoubleLinkedList<T> {
 
@@ -6,7 +10,8 @@ public class DoubleLinkedList<T> {
     private Node<T> head = null;
     private Node<T> tail = null;
 
-    private class Node<T> {
+    // internal node class to store Double LinkedList DS
+    private static class Node<T> {
         T data;
         Node<T> prev, next;
 
@@ -20,6 +25,7 @@ public class DoubleLinkedList<T> {
     public void clearList() {
         Node<T> travNode = head;
         Node<T> travNextNode;
+        // set null for all nodes for memory clear
         while (travNode.next != null) {
             travNextNode = travNode.next;
             travNode.prev = travNode.next = null;
@@ -36,6 +42,18 @@ public class DoubleLinkedList<T> {
 
     public boolean isEmptyList() {
         return size == 0;
+    }
+
+    public T lookFirst() {
+        if (isEmptyList())
+            throw new RuntimeException("List Empty");
+        return head.data;
+    }
+
+    public T lookLast() {
+        if (isEmptyList())
+            throw new RuntimeException("List Empty");
+        return tail.data;
     }
 
     public void addElementList(T elm) {
@@ -60,5 +78,127 @@ public class DoubleLinkedList<T> {
             tail = tail.next;
         }
         size++;
+    }
+
+    public int getElementIndex(Object elm) {
+        int i = 0;
+        Node<T> trav_node = head;
+        while (trav_node != null) {
+            if (elm == null && trav_node.data == null) {
+                return i;
+            } else if (elm.equals(trav_node.data)) {
+                return i;
+            }
+            trav_node = trav_node.next;
+            i++;
+        }
+
+        return -1;
+    }
+
+    // internal function to remove node
+    private T removeElement(Node<T> nodeElm) {
+
+        // check if list is empty
+        if (isEmptyList())
+            throw new RuntimeException("List Empty");
+        // check if node is either head or tail node
+        if (nodeElm.prev == null)
+            return removeFirstElementList();
+        if (nodeElm.next == null)
+            return removeLastElementList();
+
+        nodeElm.next.prev = nodeElm.prev;
+        nodeElm.prev.next = nodeElm.next;
+        T data = nodeElm.data;
+
+        // cleanup node memory
+        nodeElm.next = nodeElm.prev = null;
+        nodeElm.data = null;
+        --size;
+
+        return data;
+    }
+
+    public boolean removeElement(Object elm) {
+        Node<T> travNode = head;
+        if (elm == null) {
+            for (travNode = head; travNode != null; travNode = travNode.next) {
+                if (travNode.data == null) {
+                    removeElement(travNode);
+                    return true;
+                }
+            }
+        }
+
+        else {
+            for (travNode = head; travNode != null; travNode = travNode.next) {
+                if (elm.equals(travNode.data)) {
+                    removeElement(travNode);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public T removeLastElementList() {
+
+        if (isEmptyList())
+            throw new RuntimeException("Empty List");
+        T data = tail.data;
+        tail = tail.prev;
+        --size;
+
+        if (isEmptyList())
+            head = null; // if list is empty set head to null
+        else
+            tail.next = null;
+        return data;
+    }
+
+    public T removeFirstElementList() {
+        if (isEmptyList())
+            throw new RuntimeException("Empty List");
+        T data = head.data;
+        head = head.next;
+        --size;
+
+        if (isEmptyList())
+            tail = null; // if list is empty set tail to null
+        else
+            head.prev = null;
+        return data;
+    }
+
+    public T removeElementAtList(int index) {
+        int i;
+        Node<T> travNode;
+        if (index > sizeList() || index < 0)
+            throw new IndexOutOfBoundsException("Illegal Index passed");
+        if (index < (sizeList() / 2)) {
+            for (i = 0, travNode = head; i != index; i++) {
+                travNode = travNode.next;
+            }
+        } else {
+            for (i = sizeList() - 1, travNode = tail; i != index; i--) {
+                travNode = travNode.prev;
+            }
+        }
+        return removeElement(travNode);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        Node<T> trav = head;
+        while (trav != null) {
+            sb.append(trav.data + ", ");
+            trav = trav.next;
+        }
+
+        return sb.substring(0, sb.length() - 2) + " ]";
     }
 }
